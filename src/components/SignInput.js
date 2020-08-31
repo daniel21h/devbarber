a8452bba39d93c337eae35dd434ca7fdb66de379
daniel21h <hesseldaniel21@gmail.com>
2020-08-31 20:25:48 -0300

    feat: create inputarea component

---
+++ src/components/SignInput.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const InputArea = styled.View`
+  width: 100%;
+  height: 60px;
+  background-color: #83d6e3;
+  flex-direction: row;
+  border-radius: 10px;
+  padding-left: 15px;
+  align-items: center;
+  margin-bottom: 15px;
+`;
+
+const Input = styled.TextInput`
+  flex: 1;
+  font-size: 16px;
+  color: #268596;
+  margin-left: 10px;
+`;
+
+export default ({Icon, placeholder, value, onChangeText, password}) => {
+  return (
+    <InputArea>
+      <Input
+        placeholder={placeholder}
+        placeholderTextColor="#268596"
+        value={value}
+        onChangeText={onChangeText}
+        secureTextEntry={password}
+      />
+    </InputArea>
+  );
+};
