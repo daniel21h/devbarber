5446764e31974ba146b7c2a71fde25b6d5fd29da
daniel21h <hesseldaniel21@gmail.com>
2020-08-31 20:27:33 -0300

    feat: create signUp screen

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -1,12 +1,81 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
-import {Container} from './styles';
+import Api from '../../services/api';
+
+import SignInput from '../../components/SignInput';
+
+import {
+  Container,
+  InputArea,
+  CustomButton,
+  CustomButtonText,
+  SignMessageButton,
+  SignMessageButtonText,
+  SignMessageButtonTextBold,
+} from './styles';
 
 export default () => {
+  const navigation = useNavigation();
+
+  const [emailField, setEmailField] = useState('');
+  const [passwordField, setPasswordField] = useState('');
+  const [nameField, setNameField] = useState('');
+
+  const handleSignClick = async () => {
+    if (nameField != '' && emailField != '' && passwordField != '') {
+      let response = await Api.signUp(nameField, emailField, passwordField);
+
+      if (response.token) {
+        alert('DEU CERTO!');
+      } else {
+        alert('Erro: ' + response.error);
+      }
+    } else {
+      alert('Preencha os campos!');
+    }
+  };
+
+  const handleMessageButtonClick = () => {
+    navigation.reset({
+      routes: [{name: 'SignIn'}],
+    });
+  };
+
   return (
     <Container>
-      <Text>SignUp</Text>
+      <Text style={{fontSize: 24, color: '#fff', fontWeight: 'bold'}}>
+        Crie sua conta
+      </Text>
+
+      <InputArea>
+        <SignInput
+          placeholder="Digite seu nome"
+          value={nameField}
+          onChangeText={setNameField}
+        />
+        <SignInput
+          placeholder="Digite seu e-mail"
+          value={emailField}
+          onChangeText={setEmailField}
+        />
+        <SignInput
+          placeholder="Digite sua senha"
+          value={passwordField}
+          onChangeText={setPasswordField}
+          password={true}
+        />
+
+        <CustomButton onPress={handleSignClick}>
+          <CustomButtonText>CADASTRAR</CustomButtonText>
+        </CustomButton>
+      </InputArea>
+
+      <SignMessageButton onPress={handleMessageButtonClick}>
+        <SignMessageButtonText>Já possui uma conta?</SignMessageButtonText>
+        <SignMessageButtonTextBold>Login</SignMessageButtonTextBold>
+      </SignMessageButton>
     </Container>
   );
 };
